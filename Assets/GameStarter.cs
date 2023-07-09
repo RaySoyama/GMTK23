d7609c7fe7cf8a6edbdd679af973fa0a6eaab9d2
Alice Bottino <s.trippstr@gmail.com>
2023-07-08 18:28:18 -0700

    Update GameStarter.cs

--- Assets/GameStarter.cs
+++ Assets/GameStarter.cs
@@ -11,11 +11,12 @@ public class GameStarter : MonoBehaviour
         defblend = brain.m_DefaultBlend;
         brain.m_DefaultBlend = new Cinemachine.CinemachineBlendDefinition(Cinemachine.CinemachineBlendDefinition.Style.Cut, 0f);
         brain.m_CameraCutEvent.AddListener(OnCut);
-        cam.gameObject.SetActive(false);
+        cam.SetActive(false);
     }
     public static Cinemachine.CinemachineBlendDefinition defblend;
     public static void OnCut(Cinemachine.CinemachineBrain brain)
     {
         brain.m_DefaultBlend = defblend;
+        brain.m_CameraCutEvent.RemoveListener(OnCut);
     }
 }
